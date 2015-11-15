1d5639d095cc20c05c160e2b36345ce570823e7d
DudaGod <dyda06@mail.ru>
2015-11-16 00:12:05 +0300

    task is done

---
+++ Gruntfile.js
@@ -0,0 +1,71 @@
+module.exports = function(grunt) {
+
+require('load-grunt-tasks')(grunt); // Load all grunt-* packages from package.json
+require('time-grunt')(grunt);       // Display the elapsed execution time of grunt tasks
+
+grunt.initConfig({
+  watch: {
+    style: {
+      files: ['*.css'],
+      options: {
+        spawn: false,
+        livereload: true
+      }
+    },
+    scripts: {
+      files: ['js/*.js'],
+      options: {
+        spawn: false,
+        livereload: true
+      }
+    },
+    images: {
+      files: ['img/*.{png,jpg,gif,svg}', 'photos/*.{jpg,webp}'],
+      options: {
+        spawn: false,
+        livereload: true
+      }
+    },
+    html: {
+      files: ['*.html'],
+      options: {
+        spawn: false,
+        livereload: true
+      }
+    }
+  },
+
+
+  browserSync: {
+    dev: {
+      bsFiles: {
+        src : [
+          '*.css',
+          'js/*.js',
+          'img/*.{png,jpg,gif,svg}',
+          'photos/*.{jpg,webp}',
+          '*.html'
+        ]
+      },
+      options: {
+        watchTask: true,
+        server: {
+          baseDir: "./"
+        },
+        ghostMode: {
+          clicks: true,
+          forms: true,
+          scroll: false
+        }
+      }
+    }
+  }
+});
+
+
+  grunt.registerTask('default', [
+    'browserSync',
+    'watch'
+  ]);
+
+};
